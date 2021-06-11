7cb31ab5631be606697cd0d5863d569324481b7d
Sumi <ksm2920@gmail.com>
2021-06-11 13:32:34 +0200

    first commit

---
+++ src/models/Animal.ts
@@ -0,0 +1,8 @@
+export class Animal {
+    constructor(
+        public id: number,
+        public name: string,
+        public shortDescription: string,
+        public imageUrl: string
+    ) {}
+}
